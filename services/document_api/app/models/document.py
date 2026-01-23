bfe6237e7ed1a62c7bc27b59ddd71c7cede8ba5c
Yakub Jesawada <yakubjesawada.official@gmail.com>
2026-01-23 17:09:20 +0530

    turn status and category field into enums and add new ai metadata field and crate migration

--- services/document_api/app/models/document.py
+++ services/document_api/app/models/document.py
@@ -1,9 +1,26 @@
 # app/models/document.py
 import uuid
-from typing import Optional, List
-from datetime import datetime
-from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List, Dict, Any
+from sqlmodel import Field, Relationship
 from .base import IDMixin, TimeMixin, BaseModel
+from sqlalchemy import Enum as SAEnum
+from enum import Enum
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy import Column
+
+class UploadStatus(str, Enum):
+    UPLOADED = "uploaded"
+    PROCESSING = "processing"
+    COMPLETED = "completed"
+    FAILED = "failed"
+
+
+class DocumentCategory(str, Enum):
+    RESUME = "resume"
+    LEGAL = "legal"
+    MEDICAL = "medical"
+    FINANCIAL = "financial"
+    OTHER = "other"
 
 
 
@@ -26,11 +43,25 @@ class Document(IDMixin, TimeMixin, BaseModel, table=True):
     user_id: int = Field(foreign_key="users.id")
 
     filename: str
-    file_type: str                     # pdf, jpg, docx
-    document_category: str             # resume, legal, medical, etc
-    upload_status: str                 # uploaded, processing, completed, failed
-    storage_uri: str                   # s3/minio path
+    file_type: str                         # pdf, jpg, docx
+    document_category: DocumentCategory = Field(
+    sa_column=Column(
+            SAEnum(DocumentCategory, native_enum=False)
+        ),
+        default=DocumentCategory.OTHER
+    )
+    upload_status: UploadStatus = Field(
+        sa_column=Column(
+            SAEnum(UploadStatus, native_enum=False)
+        ),
+        default=UploadStatus.UPLOADED
+    )
+    storage_uri: str
     page_count: Optional[int] = None
+    ai_metadata: Dict[str, Any] = Field(
+        sa_column=Column(JSONB),
+        default_factory=dict
+    )
 
     collections: List["Collection"] = Relationship(
         back_populates="documents",
